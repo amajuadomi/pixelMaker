ceb929d689afacab671792bc73d7b4c48eb0dee1
Amaju Adomi <40108464+amajuadomi@users.noreply.github.com>
2018-06-30 18:24:26 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,38 +1,46 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
-let gridHeight, gridWidth, color;
+// Get the number of rows and columns from user input
 
-//get number of rows and columns, then call makeGrid function
-$("#sizePicker").submit( function (getGridSize){
-    getGridSize.preventDefault(); //
-    gridHeight = $("#inputHeight").val();
-    gridWidth = $("#inputWeight").val();
-    makeGrid(gridHeight, gridWidth);
-})
+$( document ).ready(function() {
 
-/*Create grid by creating table rows "tr" and inputting columns "td" in them
- based on the values passed in makeGrid function */
-function makeGrid(gridHeight, gridWidth) {
-//remove old tables: clear past data
-    $("tr").remove();
-//add new table: grid
-    for (let rows = 1; rows<=gridHeight; rows++ ){
-      //add rows and class
-        $("#pixelCanvas").append("<tr id = row" + rows + "></tr>");
-            for (let columns = 1; columns<=gridWidth; columns++ ){
-            //add columns inside rows
-            $("#row" +  rows).append("<td></td>");
-          }
-    }
-//assign color to grid cell on mouse click
-    $("td").click(function (){
-          // get value of color from color picker
-          color = $("#colorPicker").val();
-          // assign color to grid cell when it is clicked
-          $(this).css("background-color", color);
+  $("#sizePicker").submit(function (event){
+      // Prevent Submit Default Behaviour
+      event.preventDefault();
+      // Get value of rows
+      const gridHeight = $("#inputHeight").val();
+      // Get value of columns
+      const gridWidth = $("#inputWeight").val();
+      //Call makeGrid function
+      makeGrid(gridHeight, gridWidth);
+  })
 
-      })
+  function makeGrid(gridHeight, gridWidth) {
+  //Remove old tables: clear past data
+      $("tr").remove();
+  //Add table rows
+        for (let rows = 1; rows<=gridHeight; rows++){
+            $("#pixelCanvas").append("<tr></tr>");
+        }
+  //Add table columns
+        for (let columns = 1; columns<=gridWidth; columns++){
+            $("tr").append("<td></td>");
+        }
 
-}
+  //Assign color to grid cell on mouse click
+      $("td").click(function (){
+            // Get value of color
+            const color = $("#colorPicker").val();
+
+            // Check if the cell has a color
+            if ($(this).attr("style")){
+              // Remove color from cell if a color already exist
+              $(this).css("background-color", "");
+
+            } else {
+              // If no color exists, add color to cell
+              $(this).css("background-color", color);
+            }
+
+        })
+  }
+});
